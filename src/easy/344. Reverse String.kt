1b477c5a69fde55404b19f49688b389e302fcb38
Dogan Kilic <dnkilic@outlook.com>
2019-12-11 16:35:47 +0300

    [LC-1112]: Solved 344. Reverse String problem

---
+++ src/easy/344. Reverse String.kt
@@ -0,0 +1,10 @@
+package easy
+
+fun reverseString(s: CharArray) {
+    return s.reverse()
+}
+
+/**
+ * Runtime: 260 ms, faster than 20.18% of Kotlin online submissions for Reverse String.
+ * Memory Usage: 51.3 MB, less than 25.00% of Kotlin online submissions for Reverse String.
+ */
